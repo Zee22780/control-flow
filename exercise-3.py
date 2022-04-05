5b8a5a1d55f968dd67a58e65dfd75a32ce609e0d
zuri227 <zuri22780@gmail.com>
2022-04-04 21:21:41 -0400

    Solve exercise 3

--- exercise-3.py
+++ exercise-3.py
@@ -10,3 +10,39 @@
 #      The dog's age in dog years is xx
 
 # Hint:  Use the int() function to convert the string returned from input() into an integer
+
+
+#request input from user - it will be a number but needs to be converted to an int
+#take that int and
+  #add 10 years to first 2 years
+  #add 7 years for any remaining years
+#examples: user inputs 5
+  #year 1 = 10
+  #year 2 = 10
+  #year 3 = 7
+  #year 4 = 7
+  #year 5 = 7
+  #sum all integers = 41
+
+  #or actually!
+
+  #if int > 2 takes int and subtracts 2 and stores that in a variable
+    #mult variable by 10
+  #takes remainder and stores that in a separate variable
+    #mult variable by 7
+  #add var 1 + var 2 for total age
+  #elif int < 2 multiply by 10
+#prints a message to user and includes total age
+
+
+
+human_age = input("Input a dog's age in human years: ")
+
+if(int(human_age) > 2):
+  a = 2
+  b = int(human_age) - 2
+  dog_age = (a*10) + (b*7)
+  print(f"The dog's age in dog years is {dog_age}")
+elif(int(human_age)<2):
+  dog_age = int(human_age)*10
+  print(f"The dog's age in dog years is {dog_age}")
