5df23a6f3c2c287ce645e02ae37f4a400d24f50a
zuri227 <zuri22780@gmail.com>
2022-04-05 19:54:18 -0400

    Solve exercise 4

--- exercise-4.py
+++ exercise-4.py
@@ -12,3 +12,22 @@
 #      isosceles - two sides are the same length
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+
+#Determine how python can take in 3 inputs
+#After inputs are entered, program needs to compare the sides to each other to determine what kind of triangle it is
+  #if a, b, and c are equal 
+    #print message that says it's an equilateral
+  #elif a, b, and c are uneaqual
+    #print messages that says it's a scalene
+  #else 
+    #print message that says it's an isosceles
+
+
+a, b, c = input('Enter the lengths of three sides of a triangle: ').split()
+
+if a == b and b == c:
+  print(f'A triangle with sides of {a}, {b} & {c} is an equilateral triangle')
+elif a != b and b != c:
+  print(f'A triangle with sides of {a}, {b} & {c} is a scalene triangle')
+else:
+  print(f'A triangle with sides of {a}, {b} & {c} is an isosceles triangle')
