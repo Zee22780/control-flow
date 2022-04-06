f0a49a689ab41196a854a2a038a2afa674e5e253
zuri227 <zuri22780@gmail.com>
2022-04-05 20:56:35 -0400

    Solve exercise 6

--- exercise-6.py
+++ exercise-6.py
@@ -19,3 +19,37 @@
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+
+# request 2 pieces of input from user - month (using 3 letters) and day of the month
+# based on that input evaluates the season of the year
+  #if month == dec or month == jan or month == feb or month == mar
+    # and day >= 21 for dec or day == 1-30 for Jan, etc.
+      #i think we need either tuples or to zip or both? research tuples 
+  #print a message including month and day telling user the season
+
+
+
+month = input('Enter the month of the year as three characters (Jan - Dec): ')
+day = int(input('Enter the day of the month: '))
+
+
+if month in ('Jan', 'Feb', 'Mar'):
+  season = 'Winter'
+elif month in ('Apr', 'May', 'Jun'):
+  season = 'Spring'
+elif month in ('Jul', 'Aug', 'Sep'):
+  season = 'Summer'
+else: 
+  season = 'Fall'
+
+if (month == 'Mar') and (day >=20):
+  season = 'Spring'
+elif (month == 'Jun') and (day >=21):
+  season = 'Summer'
+elif (month == 'Sep') and (day >= 22):
+  season = 'Fall'
+elif (month == 'Dec') and (day >= 21):
+  season = 'Winter'
+  
+print(f'{month} {day} is in {season}')
+
